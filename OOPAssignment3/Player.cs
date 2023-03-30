f4493e7e08e0d5f15de96f26809fddb0cc2636c0
Ugyen Lama <ugyenchheda@gmail.com>
2023-03-31 00:33:44 +0300

    Add project files.

---
+++ OOPAssignment3/Player.cs
@@ -0,0 +1,233 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace OOPAssignment3
+{
+    internal class Player
+    {
+        public Player(string? name, DateTime? dateOfRegistration)
+        {
+            Name = name;
+            DateOfRegistration = dateOfRegistration;
+        }
+
+        public string? Name { get; set; }
+        public DateTime? DateOfRegistration { get; set; }
+
+        public void PrintTheContents(string path)
+        {
+            Console.WriteLine();
+            List<Player> players;
+
+            using (StreamReader streamReader = new StreamReader(path))
+            {
+                var jsonString = streamReader.ReadToEnd();
+                //Deserialize the JSON data into generic list type Client objects:
+                players = JsonConvert.DeserializeObject<List<Player>>(jsonString);
+            }
+            foreach (var item in players)
+            {
+                Console.WriteLine("Name: {0}, date of registration: {1}.",
+                    item.Name, item.DateOfRegistration.Value.ToString("d"));
+            }
+        }
+
+
+
+
+        public void AddPlayer(string path, Player newPlayer)
+        {
+            Console.WriteLine();
+            List<Player> players;
+
+            using (StreamReader streamReader = new StreamReader(path))
+            {
+                var jsonString = streamReader.ReadToEnd();
+                //Deserialize the JSON data into generic list type Client objects:
+                players = JsonConvert.DeserializeObject<List<Player>>(jsonString);
+            }
+
+            //Add newClient to the end of the list:
+            players.Add(newPlayer);
+
+            //true: append data to the file, false: overwrite the file
+            //If the specified file does not exist, this parameter has no effect. The
+            //constructor creates a new file.
+            using (StreamWriter streamWriter = new StreamWriter(path, false))
+            {
+                string jsonString = JsonConvert.SerializeObject(players);
+                streamWriter.Write(jsonString);
+            }
+        }
+
+
+
+
+        public void ModifyInformation(string path, string name)
+        {
+            bool ifChanged = false;
+            Console.WriteLine();
+            List<Player> players;
+
+            using (StreamReader streamReader = new StreamReader(path))
+            {
+                var jsonString = streamReader.ReadToEnd();
+                //Deserialize the JSON data into generic list type Client objects:
+                players = JsonConvert.DeserializeObject<List<Player>>(jsonString);
+
+                var chosen = players.Where(cli => cli.Name.Contains(name));
+                int numberOfPlayers = players.Count;
+
+                if (chosen.Any())
+                {
+                    //You enter here if there were wanted names...
+                    for (int i = 0; i < numberOfPlayers; i++)
+                    {
+                        if (players[i].Name.Contains(name))
+                        {
+                            Console.WriteLine("There is a customer {0} in the file.",
+                                players[i].Name);
+                            Console.Write("Do you want to change the information? (Y/N): ");
+                            string choice = Console.ReadLine().ToUpper();
+                            if (choice.StartsWith("Y"))
+                            {
+                                //You are only here if changing data is selected
+                                Console.WriteLine("Next, we go through all the information.");
+                                Console.WriteLine("If you do not change any information,press ENTER at that point.");
+                                Console.WriteLine("Current name is {0}.", players[i].Name);
+                                Console.Write("Enter new name: ");
+                                string newName = Console.ReadLine();
+                                if (!String.IsNullOrEmpty(newName))
+                                {
+                                    players[i].Name = newName;
+                                    ifChanged = true;
+                                }
+
+
+
+                                DateTime newRegistrationDate;
+                                Console.WriteLine("Current registration date is {0}.",
+                                    players[i].DateOfRegistration);
+                                Console.Write("Enter new date of registration: ");
+                                string received = Console.ReadLine();
+                                if (!String.IsNullOrEmpty(received))
+                                {
+                                    while (!DateTime.TryParse(received, out newRegistrationDate))
+                                    {
+                                        Console.Write("Not valid, try again: ");
+                                        received = Console.ReadLine();
+                                    }
+                                    players[i].DateOfRegistration = newRegistrationDate;
+                                    ifChanged = true;
+                                }
+                            }
+                        }
+                    }
+                    if (!ifChanged)
+                        Console.WriteLine("You selected the change option, but no changes were made in the end.");
+                }
+                else
+                {
+                    Console.WriteLine("No customer with that name was found.");
+                    Console.WriteLine();
+                }
+            }
+            if (ifChanged)
+            {
+                //true: append data to the file, false: overwrite the file
+                //If the specified file does not exist, this parameter has no effect. The
+                //constructor creates a new file.
+                using (StreamWriter streamWriter = new StreamWriter(path, false))
+                {
+                    string jsonString = JsonConvert.SerializeObject(players);
+                    streamWriter.Write(jsonString);
+                }
+                Console.WriteLine("Editing of data in the file was successful.");
+                Console.WriteLine();
+            }
+        }
+
+
+
+        public void RemovePlayer(string path, string name)
+        {
+            bool ifChanged = false;
+            Console.WriteLine();
+            List<Player> players;
+
+            using (StreamReader streamReader = new StreamReader(path))
+            {
+                var jsonString = streamReader.ReadToEnd();
+                //Deserialize the JSON data into generic list type Client objects:
+                players = JsonConvert.DeserializeObject<List<Player>>(jsonString);
+
+                //Are there any clients with that name?
+                var chosen = players.Where(cli => cli.Name.Contains(name));
+                int numberOfPlayers = players.Count;
+
+                if (chosen.Any())
+                {
+                    //You enter here if there were wanted names:
+                    for (int i = 0; i < numberOfPlayers; i++)
+                    {
+                        if (players[i].Name.Contains(name))
+                        {
+                            Console.WriteLine("There is a customer {0} in the file.",
+                                players[i].Name);
+                            Console.Write("Do you want to delete the customer's data? (Y/N): ");
+                            string choice = Console.ReadLine().ToUpper();
+                            if (choice.StartsWith("Y"))
+                            {
+                                //You are here only if data deletion is selected:
+                                string toBeRemoved = players[i].Name;
+                                bool succeeded = players.Remove(players[i]);
+                                if (succeeded)
+                                {
+                                    Console.WriteLine("The information of client {0} was removed from the generic collection.",
+                                        toBeRemoved);
+                                    numberOfPlayers = numberOfPlayers - 1;
+                                    i = i - 1;
+                                    ifChanged = true;
+                                }
+
+
+
+                                else
+                                    Console.WriteLine("The customer's {0} information could not be deleted from the file.",
+                                    toBeRemoved);
+                            }
+                        }
+                    }
+                    if (!ifChanged)
+                        Console.WriteLine("You chose the delete option, but nothing was done in the end.");
+                }
+                else
+                {
+                    Console.WriteLine("No customer with that name was found.");
+                    Console.WriteLine();
+                }
+            }
+            if (ifChanged)
+            {
+                //true: append data to the file, false: overwrite the file
+                //If the specified file does not exist, this parameter has no effect. The
+                //constructor creates a new file.
+                using (StreamWriter streamWriter = new StreamWriter(path, false))
+                {
+                    string jsonString = JsonConvert.SerializeObject(players);
+                    streamWriter.Write(jsonString);
+                }
+                Console.WriteLine("Deleting of data in the file was successful.");
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ OOPAssignment3/Program.cs
@@ -0,0 +1,131 @@
+﻿using OOPAssignment3;
+
+bool more = false;
+int choice;
+string path;
+string playerName;
+DateTime registrationDate;
+
+Player player;
+
+List<Player> allPlayers = new List<Player>()
+{
+    new Player(null, null)
+};
+
+do
+{
+    Console.WriteLine();
+    Console.WriteLine("Welcome to JSON file processing: Select an action(1 - 4)");
+    Console.WriteLine("1. Print the contents of the file to the screen.");
+    Console.WriteLine("2. Add a new Player.");
+    Console.WriteLine("3. Modify Player information.");
+    Console.WriteLine("4. Remove Player.");
+    Console.WriteLine();
+    Console.Write("Select an action: ");
+
+    string received = Console.ReadLine();
+    while (!Int32.TryParse(received, out choice) || choice < 1 || choice > 4)
+    {
+        Console.Write("Not valid, try again: ");
+        received = Console.ReadLine();
+    }
+
+    switch (choice)
+    {
+        case 1:
+            Console.WriteLine("Your choice: Print the contents of the file to the screen.");
+            do
+            {
+                Console.Write("Enter the file path: ");
+                path = Console.ReadLine();
+                if (String.IsNullOrEmpty(path))
+                    Console.WriteLine("The field was left empty, try again!");
+
+            } while (String.IsNullOrEmpty(path));
+
+
+            allPlayers[0].PrintTheContents(path);
+            break;
+        case 2:
+            Console.WriteLine("Your choice: Add a new Player.");
+            do
+            {
+                Console.Write("Enter the file path: ");
+                path = Console.ReadLine();
+                if (String.IsNullOrEmpty(path))
+                    Console.WriteLine("The field was left empty, try again!");
+
+            } while (String.IsNullOrEmpty(path));
+            do
+            {
+                Console.Write("Enter the Player's name: ");
+                playerName = Console.ReadLine();
+                if (String.IsNullOrEmpty(playerName))
+                    Console.WriteLine("The field was left empty, try again!");
+
+            } while (String.IsNullOrEmpty(playerName));
+
+            Console.Write("Enter the date of registration: ");
+            received = Console.ReadLine();
+            while (!DateTime.TryParse(received, out registrationDate))
+            {
+                Console.Write("Not valid, try again: ");
+                received = Console.ReadLine();
+            }
+            player = new Player(playerName, registrationDate);
+            allPlayers[0].AddPlayer(path, player);
+            break;
+        case 3:
+            Console.WriteLine("Your choice: Modify Player information.");
+            do
+            {
+                Console.Write("Enter the file path: ");
+                path = Console.ReadLine();
+                if (String.IsNullOrEmpty(path))
+                    Console.WriteLine("The field was left empty, try again!");
+
+            } while (String.IsNullOrEmpty(path));
+
+
+
+            do
+            {
+                Console.Write("Enter the Player's name: ");
+                playerName = Console.ReadLine();
+                if (String.IsNullOrEmpty(playerName))
+                    Console.WriteLine("The field was left empty, try again!");
+
+            } while (String.IsNullOrEmpty(playerName));
+            allPlayers[0].ModifyInformation(path, playerName);
+            break;
+        case 4:
+            Console.WriteLine("Your choice: Remove Player.");
+            do
+            {
+                Console.Write("Enter the file path: ");
+                path = Console.ReadLine();
+                if (String.IsNullOrEmpty(path))
+                    Console.WriteLine("The field was left empty, try again!");
+            } while (String.IsNullOrEmpty(path));
+            do
+            {
+                Console.Write("Enter the Player's name: ");
+                playerName = Console.ReadLine();
+                if (String.IsNullOrEmpty(playerName))
+                    Console.WriteLine("The field was left empty, try again!");
+            } while (String.IsNullOrEmpty(playerName));
+            allPlayers[0].RemovePlayer(path, playerName);
+            break;
+        default:
+            Console.WriteLine("Your choice: You'll never get there.");
+            break;
+    }
+    Console.WriteLine();
+    Console.Write("Do you continue with the new operation? (Y/N): ");
+    received = Console.ReadLine().ToUpper();
+    if (received.StartsWith("Y"))
+        more = true;
+    else
+        more = false;
+} while (more);
